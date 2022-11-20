662999f434af50f71a2cf0d95d578465f262608f
Jonathan Irhodia <jonathanirhodia@gmail.com>
2022-11-20 10:14:14 +0100

    Update auth0Authorizer.ts

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -11,7 +11,7 @@ const logger = createLogger('auth')
 
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = 'dev-9z7q7owf.us.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-9z7q7owf.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
